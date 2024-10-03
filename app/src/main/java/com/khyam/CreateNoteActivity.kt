00c2843ddf35d2b31e8e2463aee6001f19bc5540
khyamadhikari <kadhikar@my.centennialcollege.ca>
2024-10-03 16:41:37 -0400

    Initial commit

--- app/src/main/java/com/khyam/CreateNoteActivity.kt
+++ app/src/main/java/com/khyam/CreateNoteActivity.kt
@@ -1,12 +1,21 @@
 package com.khyam
 
-import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
@@ -17,7 +26,7 @@ class CreateNoteActivity : ComponentActivity() {
         setContent {
             CreateNoteScreen(onSaveNote = { note ->
                 MainActivity.notes.add(note)
-                setResult(Activity.RESULT_OK)
+                setResult(RESULT_OK)
                 finish()
             })
         }
